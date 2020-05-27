29bd826419518a54655db9bb7ceac4a8599ccce4
szk18 <suzuki.kensuke.tp@alumni.tsukuba.ac.jp>
2020-05-28 01:29:20 +0900

    test: expext failed github actions

--- src/pages/inspire.vue
+++ src/pages/inspire.vue
@@ -1,13 +1,14 @@
 <template>
   <section class="section">
-    <h2 class="title is-3 has-text-grey">
+		    <h2 class="title is-3 has-text-grey">
       "Just start <b-icon icon="rocket" size="is-large" />"
     </h2>
     <h3 class="subtitle is-6 has-text-grey">
       Author:
       <a href="https://github.com/anteriovieira">
-        Antério Vieira
+       			 Antério Vieira
       </a>
-    </h3>
+   		 </h3>
+    <br>
   </section>
 </template>
