f712983566d1cace6c5b2f330ad19b452f468242
marie-prog0627 <marie.726project@gmail.com>
2020-06-03 17:51:18 +0900

    add search page with query

---
+++ src/pages/article-search.vue
@@ -0,0 +1,57 @@
+<template>
+  <section class="section">
+    <h1 class="title is-2">Search Article</h1>
+    <b-field grouped>
+      <b-input v-model="word" placeholder="Serach......" expanded></b-input>
+      <p class="control">
+        <button @click="searchApi" class="button is-primary">Search</button>
+      </p>
+    </b-field>
+    <div class="list is-hoverable">
+      <div v-for="data in datas" :key="data.Ti" class="list-item">
+        {{ data.Ti }}
+        ({{ data.Y }})
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  name: 'SearchPage',
+  components: {},
+  data() {
+    return {
+      word: '',
+      params: {
+        model: 'latest',
+        count: '10',
+        offset: '0',
+        orderby: '',
+        attributes: 'Ti,Y,AA.AuN,AW'
+      },
+      datas: []
+    }
+  },
+  methods: {
+    async searchApi() {
+      const q = require('querystring').stringify(this.params)
+      const header = {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Ocp-Apim-Subscription-Key': process.env.subscriptionKey
+      }
+      const url =
+        'https://api.labs.cognitive.microsoft.com/academic/v1.0/evaluate?expr=' +
+        this.word +
+        '&' +
+        q
+
+      const result = await axios.get(url, { headers: header })
+      console.log(result.data.entities)
+
+      this.datas = result.data.entities
+    }
+  }
+}
+</script>
