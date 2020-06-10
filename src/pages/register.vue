f96624066e47d43d7c1ed80276b17ecd97441b3f
szk18 <suzuki.kensuke.tp@alumni.tsukuba.ac.jp>
2020-06-10 14:48:32 +0900

    add: firestoreに登録できるように

---
+++ src/pages/register.vue
@@ -0,0 +1,56 @@
+<template>
+  <section class="section">
+    <h1>ここはスライド内容を投稿するページ</h1>
+    <div>
+      <!-- <b-field label="論文タイトル">
+        <b-input v-model="field.inf.name"></b-input>
+      </b-field>
+      <b-field label="ようやく">
+        <b-input v-model="feild.inf.abstract"></b-input>
+      </b-field> -->
+      <b-field label="著者">
+        <b-input v-model="field.auther"></b-input>
+      </b-field>
+      <!-- <b-field label="こんとりびゅーしょん">
+        <b-input v-model="field.inf.contribution"></b-input>
+      </b-field> -->
+    </div>
+    <button @click="submit()">投稿する</button>
+  </section>
+</template>
+
+<script>
+import { db } from '~/plugins/firebaseSettings'
+
+export default {
+  name: 'HomePage',
+  components: {},
+  data() {
+    return {
+      field: {
+        auther: '',
+        inf: {
+          abstract: '',
+          contribution: '',
+          tags: [],
+          name: ''
+        }
+      }
+    }
+  },
+  methods: {
+    submit() {
+      db.collection('article')
+        .add({
+          author: 'suzuki kensuke'
+        })
+        .then((docRef) => {
+          console.log('document written with id: ' + docRef.id)
+        })
+        .catch((err) => {
+          console.log('error adding document: ' + err)
+        })
+    }
+  }
+}
+</script>
--- src/plugins/firebaseSettings.js
+++ src/plugins/firebaseSettings.js
@@ -21,3 +21,4 @@ if (!firebase.apps.length) {
 
 export default firebase
 export const auth = firebase.auth()
+export const db = firebase.firestore()
