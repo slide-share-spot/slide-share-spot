ec3b4247ab629f9508ec348c02afbef4a8e2807c
szk18 <suzuki.kensuke.tp@alumni.tsukuba.ac.jp>
2020-06-12 17:06:15 +0900

    fix: 論文infoを登録

--- src/pages/register.vue
+++ src/pages/register.vue
@@ -5,15 +5,18 @@
       <b-field label="論文タイトル">
         <b-input v-model="field.title"></b-input>
       </b-field>
-      <!--  <b-field label="ようやく">
-        <b-input v-model="feild.inf.abstract"></b-input>
-      </b-field> -->
-      <b-field label="著者">
-        <b-input v-model="field.auther"></b-input>
+      <b-field label="ようやく">
+        <b-input v-model="field.abstract"></b-input>
       </b-field>
-      <!-- <b-field label="こんとりびゅーしょん">
-        <b-input v-model="field.inf.contribution"></b-input>
-      </b-field> -->
+      <!-- <b-field label="著者">
+        <b-input v-model="field.auther"></b-input>
+      </b-field>-->
+      <b-field label="こんとりびゅーしょん">
+        <b-input v-model="field.contribution"></b-input>
+      </b-field>
+      <b-field label="verify">
+        <b-input v-model="field.verify"></b-input>
+      </b-field>
     </div>
     <button @click="submit()">投稿する</button>
   </section>
@@ -28,25 +31,35 @@ export default {
   data() {
     return {
       field: {
-        auther: '',
-        title: ''
+        title: '',
+        abstract: '',
+        contribution: '',
+        verify: ''
+        // auther: [],
+        // tags: []
       }
     }
   },
   methods: {
     submit() {
       db.collection('article')
-        .add({
-          author: this.field.auther,
-          inf: {
-            name: this.field.title
+        .doc(this.field.title)
+        .set({
+          // author: this.field.auther,
+          info: {
+            abstract: this.field.abstract,
+            contribution: this.field.contribution,
+            verify: this.field.verify
+            // tags: this.field.tags
           }
         })
         .then((docRef) => {
           console.log('document written with id: ' + docRef.id)
+          alert('success, you registered data.')
         })
         .catch((err) => {
           console.log('error adding document: ' + err)
+          alert('error, you faild.')
         })
     }
   }
