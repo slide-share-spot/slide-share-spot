d9a8e37246278a3efedb736613742e7cd764182a
szk18 <suzuki.kensuke.tp@alumni.tsukuba.ac.jp>
2020-06-24 14:08:28 +0900

    update: プレビューの表示、firebaseへのアップロード

--- src/pages/upload-file.vue
+++ src/pages/upload-file.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="container">
     <h1>ファイルをアップロードするぞ！</h1>
-    <input type="file" accept="image/*" />
+    <input type="file" accept="image/*" @change="uploadedFile" />
+    <img :src="imagePath" />
     <button @click="upload()">アップロード</button>
   </div>
 </template>
@@ -10,14 +11,30 @@
 import { storage } from '~/plugins/firebaseSettings'
 
 export default {
+  data() {
+    return {
+      file: null,
+      imagePath: null
+    }
+  },
   methods: {
+    uploadedFile(e) {
+      const file = e.target.files[0]
+      this.file = file
+      const reader = new FileReader()
+      reader.readAsDataURL(file)
+      reader.onload = () => {
+        this.imagePath = reader.result
+      }
+    },
     async upload() {
       try {
-        const storageRef = storage.ref()
-        const file = ''
-        await storageRef.put(file)
+        const storageRef = storage.ref(this.file.name)
+        await storageRef.put(this.file)
         console.log('アップロードが終わりました')
-      } catch (error) {}
+      } catch (error) {
+        console.log(error)
+      }
     }
   }
 }
