82fb961017f0c3c09dae90b9eaf43e43fa222867
szk18 <suzuki.kensuke.tp@alumni.tsukuba.ac.jp>
2020-07-03 17:43:06 +0900

    add: /post にmiddlewareを適用

--- src/middleware/authenticated.js
+++ src/middleware/authenticated.js
@@ -2,6 +2,7 @@ import { auth } from '~/plugins/firebaseSettings'
 
 export default function({ redirect }) {
   auth.onAuthStateChanged((user) => {
+    console.log(user)
     if (!user) {
       return redirect('/auth/login')
     }
