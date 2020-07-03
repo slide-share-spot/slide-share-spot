ddde4685c40e76a00d69309db00004cb9f406c49
szk18 <suzuki.kensuke.tp@alumni.tsukuba.ac.jp>
2020-07-03 17:32:23 +0900

    fix: ログインの判断はonAuthStateChangedを使う

--- src/middleware/authenticated.js
+++ src/middleware/authenticated.js
@@ -1,5 +1,9 @@
-export default function({ store, redirect }) {
-  if (!store.getters.isAuthenticated) {
-    return redirect('/auth/login')
-  }
+import { auth } from '~/plugins/firebaseSettings'
+
+export default function({ redirect }) {
+  auth.onAuthStateChanged((user) => {
+    if (!user) {
+      return redirect('/auth/login')
+    }
+  })
 }
