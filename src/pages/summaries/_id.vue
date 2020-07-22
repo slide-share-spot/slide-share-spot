51dd0e6e8db6c7b63cbc4aa2401e992b0df92fff
szk18 <suzuki.kensuke.tp@alumni.tsukuba.ac.jp>
2020-07-22 15:48:11 +0900

    wip: asyncDataで要約の取得ができる

---
+++ src/pages/summaries/_id.vue
@@ -0,0 +1,27 @@
+<template>
+  <section class="section">
+    <h1>this is summary page.</h1>
+    <p>id: {{ id }}</p>
+    <p>title: {{ article }}</p>
+  </section>
+</template>
+
+<script>
+import { db } from '~/plugins/firebaseSettings'
+export default {
+  async asyncData({ params }) {
+    const articleRef = db.collection('article-test')
+    const snapShot = await articleRef
+      .doc('audio substituting haptic system to aware the sound from backwards')
+      .get()
+
+    if (snapShot.exists) return { article: snapShot.data() }
+    else return { article: 'data not found.' }
+  },
+  data() {
+    return {
+      id: this.$route.params.id
+    }
+  }
+}
+</script>
