815c39015809cb221a98ca17d66702a545a83c3a
szk18 <suzuki.kensuke.tp@alumni.tsukuba.ac.jp>
2020-07-22 15:59:56 +0900

    add: 画像以外の情報の表示

--- src/pages/summaries/_id.vue
+++ src/pages/summaries/_id.vue
@@ -1,8 +1,26 @@
 <template>
   <section class="section">
-    <h1>this is summary page.</h1>
-    <p>id: {{ id }}</p>
-    <p>title: {{ article }}</p>
+    <h1 class="title is-2">{{ article.title }}</h1>
+
+    <div class="columns">
+      <div v-for="a in article.author" :key="a.name" class="column">
+        <div class="subtitle is-6">
+          {{ a.name }}
+          ({{ a.institute }})
+        </div>
+      </div>
+    </div>
+
+    <div class="title is-4">Abstract</div>
+    <b-message type="is-primary">{{ article.abstract }}</b-message>
+
+    <div class="title is-4">Contribution</div>
+    <b-message type="is-primary">
+      {{ article.contribution }}
+    </b-message>
+
+    <div class="title is-4">How to Verify</div>
+    <b-message type="is-primary">{{ article.verify }}</b-message>
   </section>
 </template>
 
@@ -11,16 +29,16 @@ import { db } from '~/plugins/firebaseSettings'
 export default {
   async asyncData({ params }) {
     const articleRef = db.collection('article-test')
-    const snapShot = await articleRef
-      .doc('audio substituting haptic system to aware the sound from backwards')
-      .get()
+    // const storageRef = storage.ref()
 
-    if (snapShot.exists) return { article: snapShot.data() }
+    const snapShot = await articleRef.doc(params.id).get()
+
+    if (snapShot.exists) return { article: snapShot.data().info }
     else return { article: 'data not found.' }
   },
-  data() {
-    return {
-      id: this.$route.params.id
+  computed: {
+    isLogin() {
+      return this.$store.getters.isAuthenticated
     }
   }
 }
